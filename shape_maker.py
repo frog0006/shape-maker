48882940f415fdde5d60172036e1bf63fb097dda
Ryan <ryansli721@gmail.com>
2024-07-24 20:31:43 +0000

    updated

--- shape_maker.py
+++ shape_maker.py
@@ -1,11 +1,17 @@
 def main():
-    choice = input("Sides or angles? ").strip().lower()
-    
-    if choice == "sides":
-        num_sides = int(input("How many sides do you want your shape to have? "))
-        print(f"You chose to have a shape with {num_sides} sides.")
-    else:
-        print("Invalid choice. Please choose 'sides'.")
+  choice = input("Sides or angles? ").strip().lower()
+
+  if choice == "sides":
+      num_sides = int(input("How many sides do you want your shape to have? "))
+      print(f"You chose to have a shape with {num_sides} sides.")
+  elif choice == "angles":
+      num_angles = int(input("How many angles do you want your shape to have? "))
+      print(f"You chose to have a shape with {num_angles} angles.")
+  else:
+      print("Invalid answer. Please pick either 'sides' or 'angles.'")
+
+      
+
 
 if __name__ == "__main__":
-    main()
+  main()
