34d974355e6ab5fc385460e2bb1c466218955e93
Ryan <ryansli721@gmail.com>
2024-07-25 22:16:14 +0000

    updated

--- shape_maker.py
+++ shape_maker.py
@@ -1,13 +1,25 @@
+import turtle
+
+# Initializing the turtle
+t = turtle.Turtle()
+
+
 def main():
-    while True:
-        try:
-            num_sides = int(input("How many sides do you want your shape to have? "))
-            break
-        except ValueError:
-            print("Invalid input. Please enter a valid number for the sides.")
+    #    while True:
+
+    try:
+        num_sides = int(
+            input("How many sides do you want your shape to have? "))
+        #break
+    except ValueError:
+        print("Invalid input. Please enter a valid number for the sides.")
 
     color_choice = input("What color do you want your shape to be? ")
     print(f"You chose to have a {color_choice} shape with {num_sides} sides.")
+    for i in range(num_sides):
+        t.forward(50)
+        t.left(360 / num_sides)
+
 
 if __name__ == "__main__":
     main()
